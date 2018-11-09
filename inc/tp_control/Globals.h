2623dc2fc2ec98a976e2de7217443a95f66655d8
Tom Paynter <tompaynter@tdpe.co.uk>
2018-11-09 12:24:52 +0000

    Started to add modules to the build system.

--- inc/tp_control/Globals.h
+++ inc/tp_control/Globals.h
@@ -2,9 +2,9 @@
 #define tp_control_Globals_h
 
 #if defined(TP_CONTROL_LIBRARY)
-#  define TP_CONTROL_SHARED_EXPORT
+#  define TP_CONTROL_SHARED_EXPORT TP_EXPORT
 #else
-#  define TP_CONTROL_SHARED_EXPORT
+#  define TP_CONTROL_SHARED_EXPORT TP_IMPORT
 #endif
 
 #include "tp_utils/Globals.h"
